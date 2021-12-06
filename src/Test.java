3148fd228993d5b4c3fcf0e1f2e79e0d6841123b
Home <dgryzhkov@gmail.com>
2021-12-06 12:24:25 +0700

    less1

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<10; i++){
+            list.add(i);
+        }
+        System.out.println(list);
+    list.remove(5);
+        System.out.println(list);
+
+        // проводим много удалений
+        list=new LinkedList<>();
+    }
+
+    }
+
