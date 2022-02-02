bf8b4d3420982adb75310dd810dac12ed040c46e
Dgryzhkov <dgryzhkov@gmail.com>
2022-02-03 00:32:50 +0700

    multithread

--- src/Test.java
+++ src/Test.java
@@ -1,34 +1,54 @@
 
-
-import java.util.*;
-
-
 public class Test {
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+    public static void main(String[] args) throws InterruptedException {
 
-        Iterator<Integer> iterator = list.iterator();
-
-        int idx=0;
-        // Before Java 5
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-            if (idx==1)
-            iterator.remove();
-
-            idx++;
-        }
-        System.out.println(list);
-
-        //Java 5
-//        for (int x : list) {
-//
-//            System.out.println(x);
-//        }
+//        MyThread myThread = new MyThread();
+//        myThread.start();
+//        System.out.println(" 12352363");
+//        MyThread2 myThread2 = new MyThread2();
+//        myThread2.start();
+//        System.out.println("a am sleep");
+//        Thread.sleep(3000);
+//        System.out.println("a am awake");
+        Thread thread = new Thread(new Runner());
+        thread.start();
     }
 }
 
+class Runner implements Runnable{
 
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(i + 10);
+        }
+    }
+}
+
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i + 1000);
+        }
+    }
+}
+
+class MyThread2 extends Thread {
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
