e3dd2acb1408cc1794dd59177de70f2543cd4871
Dgryzhkov <dgryzhkov@gmail.com>
2022-03-16 00:11:50 +0700

    dedlock

--- src/Test.java
+++ src/Test.java
@@ -1,84 +1,123 @@
-import java.util.concurrent.*;
+import java.util.Random;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-/*        Semaphore semaphore = new Semaphore(3); // 3 разрешения
-        try {
-            semaphore.acquire();
-            semaphore.acquire();
-            semaphore.acquire();
-            System.out.println(" All permits have been acquired");
-            semaphore.acquire();
-            System.out.println("Can't reach here...");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        semaphore.release();
+        Runner runner = new Runner();
 
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runner.firstThread();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runner.secondThread();
+            }
+        });
 
-        System.out.println(semaphore.availablePermits());*/
+        thread1.start();
+        thread2.start();
 
-        ExecutorService executorService = Executors.newFixedThreadPool(200);
-        Connection connection = Connection.getConnection();
+        thread1.join();
+        thread2.join();
 
-        for (int i = 0; i < 200; i++) {
-            executorService.submit(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        connection.work();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.DAYS);
+        runner.finished();
     }
 }
 
-//синглтон
-class Connection {
-    private static Connection connection = new Connection();
+class Runner {
 
-    private int connectionsCount;
+    private Account account1 = new Account();
 
-    private Semaphore semaphore = new Semaphore(10);
+    private Account account2 = new Account();
 
-    private Connection() {
+    private Lock lock1 = new ReentrantLock();
+    private Lock lock2 = new ReentrantLock();
 
+    private void takeLocks(Lock lock1, Lock lock2) {
+        boolean firstLockTaken = false;
+        boolean secondLockTaken = false;
+
+        while (true) {
+            try {
+                firstLockTaken = lock1.tryLock();
+                secondLockTaken = lock2.tryLock();
+            } finally {
+                if (firstLockTaken && secondLockTaken) {
+                    return;
+                }
+                if (firstLockTaken) {
+                    lock1.unlock();
+                }
+                if (secondLockTaken) {
+                    lock2.unlock();
+                }
+            }
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static Connection getConnection() {
-        return connection;
+    public void firstThread() {
+        Random random = new Random();
+
+        for (int i = 0; i < 10000; i++) {
+            takeLocks(lock1, lock2);
+            try {
+                Account.transfer(account1, account2, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
+        }
     }
 
-    public void work() throws InterruptedException {
-        semaphore.acquire();
-        try {
-            doWork();
+    public void secondThread() {
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            takeLocks(lock2, lock1);
+            try {
+                Account.transfer(account2, account1, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
         }
-        finally {
-            semaphore.release();
-        }
-
     }
 
-    public void doWork() throws InterruptedException {
+    public void finished() {
+        System.out.println(account1.getBalance());
+        System.out.println(account2.getBalance());
 
-        synchronized (this){
-            connectionsCount++;
-            System.out.println(connectionsCount);
-        }
-        Thread.sleep(5000);
+        System.out.println("Total balance  " + (account1.getBalance() + account2.getBalance()));
+    }
+}
 
-        synchronized (this){
-            connectionsCount--;
+class Account {
+    private int balance = 10000;
 
-        }
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public static void transfer(Account acc1, Account acc2, int amount) {
+        acc1.deposit(amount);
+        acc2.withdraw(amount);
     }
 }
